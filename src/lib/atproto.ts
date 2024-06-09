08fda87445877cb60ad14a9cf399437d22ea8219
Ryan Skinner <ryan@42north.ca>
2024-06-09 12:54:35 -0400

    account for did:web

--- src/lib/atproto.ts
+++ src/lib/atproto.ts
@@ -1,14 +1,45 @@
 import { AtpAgent } from '@atproto/api'
 
+export async function fetchDidFromWebHandle(handle: string) {
+  try {
+    const res = await fetch(`https://${handle}/.well-known/did.json`)
+    const didDoc = await res.json()
+    if (didDoc.id.startsWith('did:web:')) {
+      return didDoc.id
+    }
+    else {
+      throw new Error('Not a did:web handle')
+    }
+  }
+  catch (error) {
+    throw new Error('Failed to resolve did:web handle')
+  }
+}
+
 export async function fetchDidFromHandle(handle: string) {
-  const res = await fetch(`https://bsky.social/xrpc/com.atproto.identity.resolveHandle?handle=${handle}`)
-  const result = await res.json()
-  return result.did
+  try {
+    return await fetchDidFromWebHandle(handle)
+  }
+  catch (error) {
+    const res = await fetch(`https://bsky.social/xrpc/com.atproto.identity.resolveHandle?handle=${handle}`)
+    const result = await res.json()
+    return result.did
+  }
 }
 
 export async function fetchDidDoc(resolvedDid: string) {
-  const res = await fetch(`https://plc.directory/${resolvedDid}`)
-  return await res.json()
+  if (resolvedDid.startsWith('did:plc:')) {
+    const res = await fetch(`https://plc.directory/${resolvedDid}`)
+    return await res.json()
+  }
+  else if (resolvedDid.startsWith('did:web:')) {
+    const domain = resolvedDid.substring(8)
+    const res = await fetch(`https://${domain}/.well-known/did.json`)
+    return await res.json()
+  }
+  else {
+    throw new Error('Unsupported DID method')
+  }
 }
 
 export async function fetchCollections(repo: string, serviceEndpoint: string) {
