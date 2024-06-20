fdd1896f313c9debb674de06d16ece1b1e03af3d
Ryan Skinner <ryan@42north.ca>
2024-06-20 16:12:23 -0400

    better error handling

--- src/app/api/proxy/route.ts
+++ src/app/api/proxy/route.ts
@@ -14,22 +14,28 @@ export async function GET(req: NextRequest) {
 
   try {
     const response = await fetch(url)
+
     if (!response.ok) {
-      console.error(`Fetch failed with status ${response.status}`)
+      console.error(`Fetch failed with status ${response.status} for URL: ${url}`)
       return new NextResponse(JSON.stringify({ error: `Fetch failed with status ${response.status}` }), {
         status: response.status,
         headers: { 'Content-Type': 'application/json' },
       })
     }
-    const data = await response.json()
+
+    const contentType = response.headers.get('Content-Type')
+
+    const isJson = contentType && contentType.includes('application/json')
+    const data = isJson ? await response.json() : await response.text()
+
     return new NextResponse(JSON.stringify(data), {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
     })
   }
   catch (error) {
-    console.error('Error in proxy route:', error as Error)
-    return new NextResponse(JSON.stringify({ error: (error as Error).message }), {
+    console.error('Error in proxy route:', error)
+    return new NextResponse(JSON.stringify({ error: 'Internal server error' }), {
       status: 500,
       headers: { 'Content-Type': 'application/json' },
     })
